d913f1916b590ab71e8f32ed749268d521ec4ebc
joseph <U20211C828@upc.edu.pe>
2024-11-13 00:42:02 -0500

    create api service to provide moviestitles,genres, recomendations of master and movies by genre

--- development/app.go
+++ development/app.go
@@ -1,43 +1,35 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
-
-	"net"
-	"os"
+	"log"
+	"net/http"
 	"recommendation-service/syncutils"
 	"strconv"
-	"strings"
 )
 
-var endProgram = false
-
-func displayRecommendations(response *syncutils.MasterRecResponse) {
-	fmt.Println("************************************")
-	fmt.Println("Recommendations for user you")
-	fmt.Println("************************************")
-	if (len(response.Recommendations)) == 0 {
-		fmt.Println("No se encontrarón recomendaciones que cumplieran con las categorías indicadas entre nuestras películas disponibles.")
-		return
-	}
-	fmt.Printf("Se encontraron las siguiente %d películas.\n", len(response.Recommendations))
-	fmt.Println("------------------------------------")
-	for _, rec := range response.Recommendations {
-		fmt.Printf("%s\n%v\nSe recomendo la película por tener un rating estimado de %f\n%s.\n", rec.Title, rec.Genres, rec.Rating, rec.Comment)
-		fmt.Println("------------------------------------")
-	}
-	fmt.Println("Presione 'Y' para salir.")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	if strings.TrimSpace(input) == "Y" {
-		endProgram = true
-	}
-
+type MovieTitleWithID struct {
+	Title string `json:"title"`
+	Id    int    `json:"id"`
 }
 
 type Genres struct {
 	Genresname []string `json:"movieGenreNames"`
+}
+
+type MoviesTitles struct {
+	Title []string `json:"movieTitles"`
+}
+
+type MoviesGenreIds struct {
+	MoviesGenreIds [][]int `json:"movieGenreIds"`
+}
+
+type MovieGenres struct {
+	Name   string   `json:"Name"`
+	Genres []string `json:"genres"`
 }
 
 func LoadGenres(genres *Genres) error {
@@ -48,25 +40,29 @@ func LoadGenres(genres *Genres) error {
 	return nil
 }
 
-func displayGenres(genres *Genres) {
-	fmt.Println("************************************")
-	fmt.Println("Available Genres")
-	fmt.Println("************************************")
-	for i, genre := range genres.Genresname {
-		fmt.Printf("%d. %s\n", i, genre)
+func LoadMoviesTitles(movies *MoviesTitles) error {
+	err := syncutils.LoadJsonFile("config/master.json", &movies)
+	if err != nil {
+		return fmt.Errorf("Error al cargar el archivo de configuración de películas: %v", err)
 	}
-	fmt.Println("************************************")
+	return nil
 }
 
-func getUserInput(prompt string) string {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print(prompt)
-	input, _ := reader.ReadString('\n')
-	return strings.TrimSpace(input)
+func LoadMoviesGenreIds(moviesGenreIds *MoviesGenreIds) error {
+	err := syncutils.LoadJsonFile("config/master.json", &moviesGenreIds)
+	if err != nil {
+		return fmt.Errorf("Error al cargar el archivo de configuración de películas: %v", err)
+	}
+	return nil
 }
 
-func menu() {
-
+func MappperMovieGenres(idTitle int, moviesTitles *MoviesTitles, moviesGenreIds *MoviesGenreIds, genres *Genres) MovieGenres {
+	movieGenres := MovieGenres{}
+	movieGenres.Name = moviesTitles.Title[idTitle]
+	for _, genreId := range moviesGenreIds.MoviesGenreIds[idTitle] {
+		movieGenres.Genres = append(movieGenres.Genres, genres.Genresname[genreId])
+	}
+	return movieGenres
 }
 
 func Banner() {
@@ -87,76 +83,179 @@ func Banner() {
 	fmt.Println("--------------------------------------------------------------------------------")
 }
 
-func createClientRecRequest(userId int, quantity int, genreIds []int) syncutils.ClientRecRequest {
-	return syncutils.ClientRecRequest{
+func createClientRecRequest(userId int, quantity int, genreIds []int) ClientRecRequest {
+	return ClientRecRequest{
 		UserId:   userId,
 		Quantity: quantity,
 		GenreIds: genreIds,
 	}
 }
 
-func idHandler() int {
-	userIdInput := getUserInput("Enter your user ID (0-1200): ")
-	userId, err := strconv.Atoi(userIdInput)
-	if err != nil || userId < 0 || userId > 1200 {
-		panic("Invalid user ID")
-	}
-	return userId
+func getMoviesTitles() []string {
+	moviesTitles := MoviesTitles{}
+	LoadMoviesTitles(&moviesTitles)
+	return moviesTitles.Title
 }
 
+func moviesTitlesHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+	titles := getMoviesTitles()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(titles)
+}
 
-func appHandler(){
-	
-	userId := idHandler()
-	Genres := Genres{}
-	err := LoadGenres(&Genres)
+func genresHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+	genres := getMoviesGenres()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(genres)
+}
+
+func getMoviesGenres() []MovieGenres {
+	moviesTitles := MoviesTitles{}
+	LoadMoviesTitles(&moviesTitles)
+	moviesGenreIds := MoviesGenreIds{}
+	LoadMoviesGenreIds(&moviesGenreIds)
+	genres := Genres{}
+	LoadGenres(&genres)
+	var moviesGenres []MovieGenres
+	for i := 0; i < len(moviesTitles.Title); i++ {
+		movieGenres := MappperMovieGenres(i, &moviesTitles, &moviesGenreIds, &genres)
+		moviesGenres = append(moviesGenres, movieGenres)
+	}
+	return moviesGenres
+}
+
+func MoviesGenresHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+	genres := getMoviesGenres()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(genres)
+}
+
+type ClientRecRequest struct {
+	UserId   int       `json:"userId"`
+	Quantity int       `json:"quantity"`
+	GenreIds []int     `json:"genreIds"`
+	Ratings  []float64 `json:"ratings"`
+}
+
+type MovieRatingsClient struct {
+	MovieId int `json:"movieId"`
+	Rating  int `json:"rating"`
+}
+
+type ClientRecToSend struct {
+	UserId        int                  `json:"userId"`
+	Quantity      int                  `json:"quantity"`
+	GenreIds      []int                `json:"genreIds"`
+	MoviesRatings []MovieRatingsClient `json:"moviesRatings"`
+}
+
+func MappRatingsClient(ratings []MovieRatingsClient, moviesTitles *MoviesTitles) []float64 {
+	numMovies := len(moviesTitles.Title)
+	arr := make([]float64, numMovies)
+	for _, rating := range ratings {
+		arr[rating.MovieId] = float64(rating.Rating)
+	}
+	return arr
+}
+
+func sendRequestToRecommendationService(clientRecRequest ClientRecRequest) {
+	masterIp := "172.0.20.3"
+	masterPort := 9000
+	url := fmt.Sprintf("http://%s:%d/recommendation", masterIp, masterPort)
+	jsonValue, _ := json.Marshal(clientRecRequest)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error al enviar la petición al servicio de recomendaciones: %v", err)
 	}
 
-	displayGenres(&Genres)
+	defer resp.Body.Close()
+}
 
-	genreInput := getUserInput("Enter the genre indices (comma separated): ")
-	genreIndices := strings.Split(genreInput, ",")
-	var genreIds []int
-	for _, index := range genreIndices {
-		id, err := strconv.Atoi(strings.TrimSpace(index))
-		if err == nil {
-			genreIds = append(genreIds, id)
+func sendRequestToRecommendationHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+	var clientToSend ClientRecToSend
+	err := json.NewDecoder(r.Body).Decode(&clientToSend)
+	if err != nil {
+		http.Error(w, "Error al decodificar el cuerpo de la petición", http.StatusBadRequest)
+		return
+	}
+
+	clientRecRequest := createClientRecRequest(clientToSend.UserId, clientToSend.Quantity, clientToSend.GenreIds)
+	clientRecRequest.Ratings = MappRatingsClient(clientToSend.MoviesRatings, &MoviesTitles{})
+
+	sendRequestToRecommendationService(clientRecRequest)
+
+	w.Header().Set("Content-Type", "application/json")
+}
+
+// obtner peliculas de un género
+func getMoviesByGenre(genre int) []MovieTitleWithID {
+	moviesTitles := MoviesTitles{}
+	LoadMoviesTitles(&moviesTitles)
+	moviesGenreIds := MoviesGenreIds{}
+	LoadMoviesGenreIds(&moviesGenreIds)
+	genres := Genres{}
+	LoadGenres(&genres)
+	var moviesGenres []MovieTitleWithID
+	for i := 0; i < len(moviesTitles.Title); i++ {
+		for _, genreId := range moviesGenreIds.MoviesGenreIds[i] {
+			if genreId == genre {
+				movie := MovieTitleWithID{
+					Title: moviesTitles.Title[i],
+					Id:    i,
+				}
+				moviesGenres = append(moviesGenres, movie)
+				break
+			}
 		}
 	}
+	return moviesGenres
+}
 
-	quantityInput := getUserInput("Enter the number of recommendations: ")
-	quantity, err := strconv.Atoi(quantityInput)
-	if err != nil {
-		panic("Invalid quantity")
+func getMoviesByGenresHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
 	}
-	fmt.Printf("User ID: %d\n", userId)
-	fmt.Printf("Genres: %v\n", genreIds)
-	fmt.Printf("Quantity: %d\n", quantity)
-	request := createClientRecRequest(userId, quantity, genreIds)
+	//getID of genre from request
+	genre := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(genre)
+	if err != nil {
+		http.Error(w, "Error al convertir el id del género a entero", http.StatusBadRequest)
+		return
+	}
+	movies := getMoviesByGenre(id)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
 
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "172.21.0.3", syncutils.ServicePort))
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-	err = syncutils.SendObjectAsJsonMessage(&request, &conn)
-	if err != nil {
-		panic(err)
-	}
-	var response syncutils.MasterRecResponse
-	err = syncutils.ReceiveJsonMessageAsObject(&response, &conn)
-	if err != nil {
-		panic(err)
-	}
-	displayRecommendations(&response)
+func setupRoutes() {
+	http.HandleFunc("/movies/titles", moviesTitlesHandler)
+	http.HandleFunc("/genres", genresHandler)
+	http.HandleFunc("/movies/genres", MoviesGenresHandler)
+	http.HandleFunc("/recommendations", sendRequestToRecommendationHandler)
+	http.HandleFunc("/genres/movies", getMoviesByGenresHandler)
+	log.Println("Server running on port 9000")
+	log.Fatal(http.ListenAndServe(":9000", nil))
+
 }
 
 func main() {
-
 	Banner()
-	appHandler()
-
-
+	setupRoutes()
 }
